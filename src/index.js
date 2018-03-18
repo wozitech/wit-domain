7a83451f664138f0c58aed3317a8aa7d4089edda
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2018-03-18 17:19:49 +0000

    Updating the test printMsg function to take parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
+exports.printMsg = function(message, loglevel="info") {
+    switch(loglevel) {
+        case "debug":
+            console.log("[DEBUG]: ", message);
+            break;
+        case "warn":
+            console.log("[WARN] : ", message);
+            break;
+        default:
+            console.log("[INFO] : ", message);
+    }
 }
